13c056516b8d0f89dfcc467f4b3236cd8c4a92fc
Lance0567 <esurenalance08@gmail.com>
2025-05-08 12:41:10 +0800

    dashboard update

--- Frames/uDashboard.pas
+++ Frames/uDashboard.pas
@@ -4,10 +4,41 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
-  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls;
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  System.Skia, FMX.ListView.Types, FMX.ListView.Appearances,
+  FMX.ListView.Adapters.Base, FMX.ListView, FMX.Skia, FMX.Layouts, FMX.Objects,
+  FMX.Controls.Presentation;
 
 type
   TfDashboard = class(TFrame)
+    ScrollBox1: TScrollBox;
+    lytBottom: TLayout;
+    rRecentContracts: TRectangle;
+    Layout2: TLayout;
+    SkLabel1: TSkLabel;
+    ListView1: TListView;
+    glytCards: TGridLayout;
+    rCard1: TRectangle;
+    lytCardD1: TLayout;
+    lbTotalContracts: TLabel;
+    Image1: TImage;
+    lbTotalContractsC: TLabel;
+    rCard2: TRectangle;
+    Layout1: TLayout;
+    lbActiveClients: TLabel;
+    Image2: TImage;
+    lbActiveClientsC: TLabel;
+    rCard3: TRectangle;
+    Layout3: TLayout;
+    lbFullyPaid: TLabel;
+    Image3: TImage;
+    lbFullyPaidC: TLabel;
+    rCard4: TRectangle;
+    Layout4: TLayout;
+    lbNotYetPaid: TLabel;
+    Image4: TImage;
+    lbNotYetPaidC: TLabel;
+    procedure FrameResize(Sender: TObject);
   private
     { Private declarations }
   public
@@ -17,5 +48,22 @@ type
 implementation
 
 {$R *.fmx}
+
+uses uMain;
+
+procedure TfDashboard.FrameResize(Sender: TObject);
+begin
+  // Cards responsive
+  if frmMain.ClientWidth >= 1300 then
+  begin
+    glytCards.Height := 170;
+    glytCards.ItemWidth := 272;
+  end
+  else
+  begin
+    glytCards.Height := 321;
+    glytCards.ItemWidth := 262;
+  end;
+end;
 
 end.
--- Pages/uMain.pas
+++ Pages/uMain.pas
@@ -7,7 +7,8 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
   FMX.Controls.Presentation, FMX.MultiView, System.ImageList, FMX.ImgList,
   FMX.Layouts, uDashboard, FMX.Objects, FMX.TabControl, FMX.Effects,
-  FMX.Filter.Effects;
+  FMX.Filter.Effects, FMX.ListView.Types, FMX.ListView.Appearances,
+  FMX.ListView.Adapters.Base, FMX.ListView, System.Skia, FMX.Skia;
 
 type
   TfrmMain = class(TForm)
@@ -26,14 +27,11 @@ type
     lytHeader: TLayout;
     lbTitle: TLabel;
     btnTrigger: TCornerButton;
-    ScrollBox1: TScrollBox;
-    FlowLayout1: TFlowLayout;
-    Rectangle1: TRectangle;
-    Rectangle2: TRectangle;
-    Rectangle3: TRectangle;
-    Rectangle4: TRectangle;
+    fDashboard1: TfDashboard;
+    cbtnViewAllContracts: TCornerButton;
     procedure mvSidebarResize(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
   private
     { Private declarations }
   public
@@ -52,6 +50,11 @@ uses uDm;
 procedure TfrmMain.FormCreate(Sender: TObject);
 begin
   mvSidebar.ShowMaster;
+end;
+
+procedure TfrmMain.FormResize(Sender: TObject);
+begin
+  Self.Caption := 'Main Form' + ' Height: ' + Self.Height.ToString + ' Width: ' + Self.Width.ToString;
 end;
 
 procedure TfrmMain.mvSidebarResize(Sender: TObject);
